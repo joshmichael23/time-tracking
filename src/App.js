47f783f6f42ea7e0608eca31ce7242e23145a9a5
Josh Michael Olea <34050740+joshmichael23@users.noreply.github.com>
2023-12-26 01:57:17 +0800

    2023-12-26 first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
-import './App.css';
+import userpic from './images/image-jeremy.png';
+import ellipsis from './images/icon-ellipsis.svg';
+import data from './data.json';
+import React, { useEffect, useState } from "react";
+import './style.css';
 
 function App() {
+
+  const [active, setActive] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onLoad={()=>setActive(active => 'daily')} className="App">
+
+        
+      <div className='wrapper'>
+
+        <div className='card-containers'>
+          <div className='card card-main'>
+            <div className='card-top'>
+                  <img src={userpic}></img>
+                  <h1><span>Report for</span> Jeremy Robson</h1>
+            </div>
+            <div className='card-content'>
+                <a onClick={() => setActive(active => 'daily')}>Daily</a>
+                <a onClick={() => setActive(active => 'weekly')}>Weekly</a>
+                <a onClick={() => setActive(active => 'monthly')}>Monthly</a>
+            </div>
+
+          </div>
+
+
+          {
+            data.map((answer, i) => {     
+              return(
+              <div className={'card ' + answer.title.toString().toLowerCase()}>
+                <div className='card-content'>
+                  <div className='card-content-top'>
+                    <p>{answer.title}</p>
+                    <button><img src={ellipsis}></img></button>
+                  </div>
+                  <div className='card-content-bottom'>
+                    <div className={active=="daily"? 'daily show' : 'daily'}>
+                      <h2>{answer.timeframes.daily.current}hrs</h2>
+                      <p>Yesterday - {answer.timeframes.daily.previous}hrs</p>
+                    </div>
+
+                    <div className={active=="weekly"? 'weekly show' : 'weekly'}>
+                      <h2>{answer.timeframes.weekly.current}hrs</h2>
+                      <p>Last Week - {answer.timeframes.weekly.previous}hrs</p>
+                    </div>
+
+                    <div className={active=="monthly"? 'monthly show' : 'monthly' }>
+                      <h2>{answer.timeframes.monthly.current}hrs</h2>
+                      <p>Last Month - {answer.timeframes.monthly.previous}hrs</p>
+                    </div>
+                  
+                  
+                  </div>
+                </div>
+              </div>);
+
+            })
+          
+          }
+          
+        </div>
+
+      </div>
     </div>
   );
 }
